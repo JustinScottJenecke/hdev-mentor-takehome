d58172eb7041bde566a464e4b9eccd9d3f64d129
justinscottjenecke <justinjenecke@gmail.com>
2024-07-29 18:41:06 +0200

    feature: tests and debugs creating new users in db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from 'cors';
-import mongoose from "mongoose";
+import mongoose, { Error } from "mongoose";
 import UserSchema from "./models/User.js";
 // import userModel from "./models/User.js";
 
@@ -21,16 +21,17 @@ APP.get("/", (req, res) => {
 
 // Create 
 APP.post("/api/users/register", async (req, res) => {
-    // console.log(req.body);
-    const newUserDto = {username: req.body.name, email: req.body.email,password: req.body.password};
+    console.log(req.body);
+    const newUserDto = {name: req.body.name, email: req.body.email, password: req.body.password};
 
     try {
         const user = await UserSchema.create(newUserDto);
-        res.send(user);
-        res.json({status: 'ok'});            
+        // res.send(user);
+        res.status(201).json(user);            
     
-    } catch (error) {
-        res.json({status: 'error'});    
+    } catch (err) {
+        console.error(err);
+        res.status(400).json({err, message: err.errmsg});    
     }
 });
 
@@ -40,7 +41,10 @@ APP.post("/api/users/login", async (req, res) => {
     const loginUserDto = {username: req.body.name, password: req.body.password};
     const user = await UserSchema.findOne(loginUserDto);
 
-    return user ? res.json({status:'ok', user: true}) : res.json({status: 'error', user: false});           
+    user ? 
+        res.status(200).json({user: true})
+    : 
+       console.error('user not found.'), res.status(400).json({user: false});           
 
 });
 
--- server/models/User.js
+++ server/models/User.js
@@ -8,7 +8,7 @@ const UserSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true}
 }, {
-    collection: "user-data"
+    collection: "user-collection"
 });
 
 export default mongoose.model('UserData', UserSchema);
